d2134d226aebf85434d75d924a584a548d9fa8c6
fajiculaymarkjoseph <164134528+fajiculaymarkjoseph@users.noreply.github.com>
2024-04-06 08:55:04 +0800

    ACTIVITY-7-CALCULATOR

--- src/app/calculator/calculator.page.ts
+++ src/app/calculator/calculator.page.ts
@@ -12,4 +12,67 @@ export class CalculatorPage implements OnInit {
   ngOnInit() {
   }
 
+  displayValue: string = '';
+  operand1: number = 0;
+  operator: string = '';
+  operand2: number = 0;
+
+  appendNumber(number: number) {
+    if (this.displayValue === '0') {
+      this.displayValue = number.toString();
+    } else {
+      this.displayValue += number.toString();
+    }
+  }
+  
+  add() {
+    this.operator = '+';
+    this.operand1 = parseFloat(this.displayValue);
+    this.displayValue = '0';
+  }
+
+  subtract() {
+    this.operator = '-';
+    this.operand1 = parseFloat(this.displayValue);
+    this.displayValue = '0';
+  }
+
+  multiply() {
+    this.operator = '*';
+    this.operand1 = parseFloat(this.displayValue);
+    this.displayValue = '0';
+  }
+
+  divide() {
+    this.operator = '/';
+    this.operand1 = parseFloat(this.displayValue);
+    this.displayValue = '0';
+  }
+
+  calculate() {
+    this.operand2 = parseFloat(this.displayValue);
+    switch (this.operator) {
+      case '+':
+        this.displayValue = (this.operand1 + this.operand2 ).toString();
+        break;
+      case '-':
+        this.displayValue = (this.operand1 - this.operand2).toString();
+        break;
+      case '*':
+        this.displayValue = (this.operand1 * this.operand2).toString();
+        break;
+      case '/':
+        this.displayValue = (this.operand1 / this.operand2).toString();
+        break;
+  
+    }
+    
+  }
+
+  clear() {
+    this.displayValue = '0';
+    this.operand1 = 0;
+    this.operator = '';
+    this.operand2 = 0;
+  }
 }
